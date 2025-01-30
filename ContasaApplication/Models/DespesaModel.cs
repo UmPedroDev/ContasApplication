140256371e358d0aec8304670fcea10bbe3fa7b9
Pedro.Teixeira <umpedrodev@gmail.com>
2025-01-30 15:24:03 -0300

    Criando a class DespesaModel

---
+++ ContasaApplication/Models/DespesaModel.cs
@@ -0,0 +1,14 @@
+﻿using ContasApplication.Enums;
+
+namespace ContasApplication.Models
+{
+    public class DespesaModel
+    {
+        public string NomeDespesa { get; set; } = string.Empty;
+        public double ValorDespesa { get; set; }
+        public DateTime CreateDate { get; set; }
+        public EnumSimNao Parcelado { get; set; } = EnumSimNao.Não;
+        public int QuantidadeParcelas { get; set; }
+        public int QuantidadeParcelasPagas { get; set; }
+    }
+}
