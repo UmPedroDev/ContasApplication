bf7d09884f46c27b68d126d6031d6bd53376bfcb
Pedro.Teixeira <umpedrodev@gmail.com>
2025-02-11 17:49:44 -0300

    Adicionada todas funções e view para adicionar uma despesa

---
+++ ContasaApplication/Models/DespesaAuxiliar.cs
@@ -0,0 +1,9 @@
+﻿namespace ContasApplication.Models
+{
+    public class DespesaAuxiliar
+    {
+        public List<DespesaModel> ListDespesas { get; set; }
+        public DateTime DataFiltro { get; set; }
+        public double valorTotal { get; set; }
+    }
+}
---
+++ ContasaApplication/wwwroot/js/AddDespesa.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $('#flexCheckDefault').change(function () {
+        if ($(this).is(':checked')) {
+            $('#parcelado').show();
+        }
+        else {
+            $('#parcelado').hide();
+        }
+    });
+});
+
+document.getElementById("Price").addEventListener("input", function (event) {
+    let value = event.target.value.replace(/\D/g, "");
+    if (value.length > 2) {
+        value = value.replace(/(\d)(\d{2})$/, "$1,$2");
+    }
+    event.target.value = value.replace(/(\d)(?=(\d{3})+(\D|$))/g, "$1.");
+});
+
+var cleave = new Cleave('#Price', {
+    numeral: true,
+    numeralThousandsGroupStyle: 'thousand',
+    prefix: 'R$ ',
+});
